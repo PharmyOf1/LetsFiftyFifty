8cbaa80772c2accd3cde3d306fe2fb4ea1075939
PharmyOf1 <phillip.harm@gmail.com>
2017-05-21 17:02:12 -0400

    redesign

--- letsfiftyfifty/src/main/java/phek/letsfiftyfifty/MainActivity.java
+++ letsfiftyfifty/src/main/java/phek/letsfiftyfifty/MainActivity.java
@@ -74,23 +74,22 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_me) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_privacypolicy) {
 
         }
 
