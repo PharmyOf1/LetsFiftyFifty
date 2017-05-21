c061eab77026dc2982c2c96405db901bbe1c7b60
PharmyOf1 <phillip.harm@gmail.com>
2017-05-21 17:19:18 -0400

    redesign

---
+++ letsfiftyfifty/src/main/java/phek/letsfiftyfifty/adapter/TabsPagerAdapter.java
@@ -0,0 +1,40 @@
+package phek.letsfiftyfifty.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import phek.letsfiftyfifty.fragment.FavoritesFragment;
+import phek.letsfiftyfifty.fragment.HomeFragment;
+import phek.letsfiftyfifty.fragment.MyLocationFragment;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return new HomeFragment();
+            case 1:
+                // Games fragment activity
+                return new FavoritesFragment();
+            case 2:
+                // Movies fragment activity
+                return new MyLocationFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+}
